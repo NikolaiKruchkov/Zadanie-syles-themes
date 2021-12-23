2043419af87881965114963d6113e9ddbd77562a
NikolaiKruchkov <47074017+NikolaiKruchkov@users.noreply.github.com>
2021-12-23 14:39:30 +0300

    Add files via upload

---
+++ Zadanie comandi/MainWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace Zadanie_comandi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+    
+        }
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = (sender as ComboBox).SelectedItem as String;
+            if (textBox != null)
+            {
+                textBox.FontFamily = new FontFamily(fontName);
+            }
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            double fontSize = Convert.ToDouble((sender as ComboBox).SelectedItem as String);
+            if (textBox != null)
+            {
+                textBox.FontSize = fontSize;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string weight = "Bold";
+            FontWeight fw = (FontWeight)new FontWeightConverter().ConvertFromString(weight);
+            textBox.FontWeight = fw;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            string style = "Italic";
+            FontStyle fs = (FontStyle)new FontStyleConverter().ConvertFromString(style);
+            textBox.FontStyle = fs;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            TextDecorationCollection tcd = new TextDecorationCollection(1);
+            tcd.Add(TextDecorations.Underline);
+            textBox.TextDecorations = tcd;
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                Brush bl = (Brush)new BrushConverter().ConvertFromString("Black");
+                textBox.Foreground = bl;
+            }
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                Brush rd = (Brush)new BrushConverter().ConvertFromString("Red");
+                textBox.Foreground = rd;
+            }
+        }
+
+        private void ExitExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+            }
+        }
+
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void newWindowExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Window1 newWindow = new Window1();
+            newWindow.WindowStyle = WindowStyle.ToolWindow;
+            newWindow.ShowDialog();
+        }
+
+        private void inCenterExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Double width = SystemParameters.FullPrimaryScreenWidth;
+            Double heiht = SystemParameters.FullPrimaryScreenHeight;
+            this.Top = (heiht - this.Height) / 2;
+            this.Left = (width - this.Width) / 2;
+        }
+
+        private void showErrorExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Внимание! Ошибка!", "Ошибка в программе!", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void ExitCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (textBox!=null&&textBox.Text.Length==0)
+            {
+                e.CanExecute = true;
+            }
+            else
+            {
+                e.CanExecute = false;
+            }
+
+        }
+
+        private void darkThemesExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Uri uri = new Uri("Dark.xaml",UriKind.Relative);
+            ResourceDictionary resource = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resource);
+        }
+
+        private void whiteThemesExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Uri uri = new Uri("White.xaml", UriKind.Relative);
+            ResourceDictionary resource = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resource);
+        }
+    }
+}
---
+++ Zadanie comandi/MyCommands.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Zadanie_comandi
+{
+    class MyCommands
+    {
+        public static RoutedCommand Exit { get; set; }
+        public static RoutedCommand Save { get; set; }
+        public static RoutedCommand Open { get; set; }
+        public static RoutedCommand newWindow { get; set; }
+        public static RoutedCommand inCenter { get; set; }
+        public static RoutedCommand showError { get; set; }
+        public static RoutedCommand darkThemes { get; set; }
+        public static RoutedCommand whiteThemes { get; set; }
+
+        static MyCommands()
+        {
+            InputGestureCollection input1 = new InputGestureCollection();
+            input1.Add(new KeyGesture(Key.X, ModifierKeys.Control, "Ctr+X"));
+            InputGestureCollection input2 = new InputGestureCollection();
+            input2.Add(new KeyGesture(Key.S, ModifierKeys.Control, "Ctr+S"));
+            InputGestureCollection input3 = new InputGestureCollection();
+            input3.Add(new KeyGesture(Key.O, ModifierKeys.Control, "Ctr+O"));
+            InputGestureCollection input4 = new InputGestureCollection();
+            input4.Add(new KeyGesture(Key.W, ModifierKeys.Control, "Ctr+W"));
+            InputGestureCollection input5 = new InputGestureCollection();
+            input5.Add(new KeyGesture(Key.I, ModifierKeys.Control, "Ctr+I"));
+            InputGestureCollection input6 = new InputGestureCollection();
+            input6.Add(new KeyGesture(Key.E, ModifierKeys.Control, "Ctr+E"));
+            InputGestureCollection input7 = new InputGestureCollection();
+            input7.Add(new KeyGesture(Key.D, ModifierKeys.Control, "Ctr+D"));
+            InputGestureCollection input8 = new InputGestureCollection();
+            input8.Add(new KeyGesture(Key.G, ModifierKeys.Control, "Ctr+G"));
+            Exit = new RoutedUICommand("Выход","Exit",typeof(MyCommands),  input1);
+            Save = new RoutedUICommand("Сохранить", "Save", typeof(MyCommands), input2);
+            Open = new RoutedUICommand("Открыть", "Open", typeof(MyCommands), input3);
+            newWindow = new RoutedUICommand("Новое окно", "New Window", typeof(MyCommands), input4);
+            inCenter = new RoutedUICommand("В центр", "In Center", typeof(MyCommands), input5);
+            showError = new RoutedUICommand("Показать ошибку", "Show Error", typeof(MyCommands), input6);
+            darkThemes = new RoutedUICommand("Темная тема", "Dark themes", typeof(MyCommands), input7);
+            whiteThemes = new RoutedUICommand("Светлая тема", "White themes", typeof(MyCommands), input8);
+        }
+    }
+}
